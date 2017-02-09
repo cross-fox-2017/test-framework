bafb1e3901a97764c97853cffac0e06b139b56b3
Syanmil <syanmil@yahoo.com>
2017-02-09 15:50:17 +0700

    using should for penambahan pengurangan dan perkalian

--- test/index.js
+++ test/index.js
@@ -10,33 +10,47 @@ describe('somethin', function() {
     expect('everything').to.be.ok;
   })
 })
-// everything.should.be.ok;
 // -----------------------------------------------------------------------------
 // Penambahan
 
-// expect(matematika.penambahan(1,1).to.equal(2))
-// expect(matematika.penambahan(-1,-1).to.equal(-2))
-// expect(matematika.penambahan(2*2,4*4).to.equal(20))
-// expect(matematika.penambahan("1", "2").to.equal("Input is Required and must in Number"))
-// expect(matematika.penambahan(1).to.equal("Input is Required and must in Number"))
-
+describe('fungsi penambahan', function() {
+  it('fungsi berjalan sesuai dengan fungsinya', function() {
+    expect(matematika.penambahan(1,1)).to.equal(2)
+    expect(matematika.penambahan(-1,-1)).to.equal(-2)
+    expect(matematika.penambahan(2*2,4*4)).to.equal(20)
+    })
+    it('input harus diisi dan harus angka', function() {
+    expect(matematika.penambahan("1", "2")).to.equal("Input is Required and must in Number")
+    expect(matematika.penambahan(1)).to.equal("Input is Required and must in Number")
+  })
+})
 // -----------------------------------------------------------------------------
 // pengurangan
 
-// expect(matematika.pengurangan(3,1).to.equal(2))
-// expect(matematika.pengurangan("1", "2").to.equal("Input is Required and must in Number"))
-// expect(matematika.pengurangan(1).to.equal("Input is Required and must in Number"))
-
+describe('pengurangan', function() {
+  it('fungsi berjalan sesuai dengan fungsinya', function() {
+    expect(matematika.pengurangan(3,1)).to.equal(2)
+    })
+    it('input harus diisi dan harus angka', function() {
+    expect(matematika.pengurangan("1", "2")).to.equal("Input is Required and must in Number")
+    expect(matematika.pengurangan(1)).to.equal("Input is Required and must in Number")
+  })
+})
 // -----------------------------------------------------------------------------
 // perkalian
 
-// expect(matematika.perkalian(0,0).to.equal(0))
-// expect(matematika.perkalian(0,1).to.equal(0))
-// expect(matematika.perkalian(2,2).to.equal(4))
-// expect(matematika.perkalian(-2,2).to.equal(-4))
-// expect(matematika.perkalian("9", "3").to.equal("Input is Required and must in Number"))
-// expect(matematika.perkalian(3).to.equal("Input is Required and must in Number"))
-
+describe('perkalian', function() {
+  it('fungsi berjalan sesuai dengan fungsinya', function() {
+    expect(matematika.perkalian(0,0)).to.equal(0)
+    expect(matematika.perkalian(0,1)).to.equal(0)
+    expect(matematika.perkalian(2,2)).to.equal(4)
+    expect(matematika.perkalian(-2,2)).to.equal(-4)
+    })
+    it('input harus diisi dan harus angka', function() {
+    expect(matematika.perkalian("9", "3")).to.equal("Input is Required and must in Number")
+    expect(matematika.perkalian(3)).to.equal("Input is Required and must in Number")
+  })
+})
 // -----------------------------------------------------------------------------
 // pembagian
 
